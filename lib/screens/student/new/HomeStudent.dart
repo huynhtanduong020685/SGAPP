ae5905ef43b44c02c8402a09d65376cc2511a6f8
caovananh <36398655+caovananh@users.noreply.github.com>
2021-08-16 18:29:51 +0700

    update

--- lib/screens/student/new/HomeStudent.dart
+++ lib/screens/student/new/HomeStudent.dart
@@ -260,11 +260,145 @@ class _StudentHomeState extends State<StudentHome> {
         );
         break;
       case 0:
-        return Center(
-            child: Text(
-          'Main page',
-          style: TextStyle(fontSize: 45),
-        ));
+        return ListView(
+          children: [
+            Container(
+              height: 280,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/card-thumb.png"),
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 10, bottom: 10),
+                            child: Text("25/12/2021     14:30",
+                                style: TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 280,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/card-thumb.png"),
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 10, bottom: 10),
+                            child: Text("25/12/2021     14:30",
+                                style: TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 280,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Container(
+                  alignment: Alignment.bottomCenter,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10)),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/card-thumb.png"),
+                      fit: BoxFit.fitWidth,
+                      alignment: Alignment.topCenter,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 10, bottom: 10),
+                            child: Text("25/12/2021     14:30",
+                                style: TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
         break;
       case 2:
         return Center(
